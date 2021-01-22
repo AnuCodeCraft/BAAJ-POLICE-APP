c68d82034cebbb9454f9afa5836d41f160783f18
Anupriya <anupriya10031@gmail.com>
2021-01-22 10:01:39 +0530

    frist commit

---
+++ src/screens/Event.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native'
+
+export default function Event() {
+    return(
+        <view>
+            <Text>Event</Text>
+        </view>
+    ) 
+    
+        
+
+}
---
+++ src/screens/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text } from 'react-native'
+/*import Backgroud from '../components/Background'
+import { useState } from 'react'
+import MapView, { Marker } from 'react-native-maps'
+import GetLocation from 'react-native-get-location'
+import { ScrollView } from 'react-native-gesture-handler'
+import { TextInput } from '../components/TextInput'*/
+//import { Button } from 'react-native-paper'
+
+export default function Home() {
+    return(
+        <view>
+            <Text>Home</Text>
+        </view>
+    ) 
+    
+        
+
+}
